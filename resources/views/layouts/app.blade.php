da52051f527a6b55ff46175197509a5d47c7d3aa
MuhammadJamshaid321 <muhammadjamshaidabdulrehman123@gmail.com>
2024-12-20 17:26:38 +0500

    Authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Laravel Application</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <style>
         body {
             display: flex;
@@ -18,32 +21,81 @@
             margin-top: auto;
         }
     </style>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <!-- Header Section -->
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-secondary bg-secondary white shadow-sm">
             <div class="container">
-                <a class="navbar-brand mx-auto" href="#">PhoneBook</a>
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
         </nav>
-    </header>
 
-    <!-- Main Content Section -->
-    <main>
-        @yield('content')
-    </main>
-
-    <!-- Footer Section -->
-    <footer class="bg-primary text-light py-3">
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+    <footer class="bg-secondary py-3">
         <div class="container text-center">
             <p class="mb-1">&copy; 2024 PhoneBook. Vizrex Solution (Private) Limited.</p>
-            <a href="#" class="text-decoration-none text-light me-3">Privacy Policy</a>
-            <a href="#" class="text-decoration-none text-light me-3">Terms of Service</a>
-            <a href="#" class="text-decoration-none text-light">Contact Us</a>
+            <a href="#" class="text-decoration-none text-dark me-3">Privacy Policy</a>
+            <a href="#" class="text-decoration-none text-dark me-3">Terms of Service</a>
+            <a href="#" class="text-decoration-none text-dark">Contact Us</a>
         </div>
     </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 
-Route::controller(ContactController::class)->group(function () {
-    Route::get('/contacts', 'index')->name('contacts.index');
-    Route::get('/contact/create', 'create')->name('contacts.create');
-    Route::post('/contact', 'store')->name('contacts.store');
-    Route::get('/contact/{id}/edit', 'edit')->name('contacts.edit');
-    Route::put('/contact/{id}', 'update')->name('contacts.update');
-    Route::delete('/contact/{id}', 'destroy')->name('contacts.destroy');
-    Route::get('/contact/{id}', 'show')->name('contacts.show');
+
+Route::middleware(['auth'])->controller(ContactController::class)->group(function () {
+    
+        Route::get('/contacts', 'index')->name('contacts.index');
+        Route::get('/contact/create', 'create')->name('contacts.create');
+        Route::post('/contact', 'store')->name('contacts.store');
+        Route::get('/contact/{id}/edit', 'edit')->name('contacts.edit');
+        Route::put('/contact/{id}', 'update')->name('contacts.update');
+        Route::delete('/contact/{id}', 'destroy')->name('contacts.destroy');
+        Route::get('/contact/{id}', 'show')->name('contacts.show');
+    
 });
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::redirect('/', '/contacts')->middleware('auth');
