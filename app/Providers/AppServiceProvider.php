8f0633d030fef0679b2f7cc5039dfd551de7efb1
MuhammadJamshaid321 <muhammadjamshaidabdulrehman123@gmail.com>
2024-12-27 15:39:23 +0500

    Conflict Resolve From AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,8 @@
 namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\ContactRepository;
-<<<<<<< HEAD
-=======
 use App\Facades\ContactRepositoryFacade;
 use Illuminate\Pagination\Paginator;
->>>>>>> 3d6ff3b (Facades Pattern and Pagination Added)
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -15,13 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
 {
-<<<<<<< HEAD
-    $this->app->singleton(
-        \App\Repositories\ContactRepository::class
-    );
-=======
     $this->app->singleton(ContactRepository::class);
->>>>>>> 3d6ff3b (Facades Pattern and Pagination Added)
 }
     /**
      * Bootstrap any application services.
